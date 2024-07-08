82a3a9c146bb122706bf8f667948e751a9f0e9ee
Yash Mittal <yashmittal2004@gmail.com>
2024-07-09 02:04:02 +0530

    Completed the Project

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ const http = require("http");
 const path = require("path");
 
 const socketio = require("socket.io");
-
 const server = http.createServer(app);
-
 const io = socketio(server);
 
+io.on("connection", function(socket){
+    socket.on("send-location", function (data){
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+    socket.on("disconnect", function(){
+        io.emit("user-disconnect", socket.id);
+    })
+});
+
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    res.send("Hello World");
+    res.render("index");
 })
 
 server.listen(3000);
